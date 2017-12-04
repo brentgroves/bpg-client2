3e56a13019e235cbd0b1a20cc4f24eede297249d
robogroves <brent.groves@gmail.com>
2017-12-04 18:53:04 -0500

    12/4/2017 added semantic

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, {Component} from "react";
+import { Panel} from "react-bootstrap";
+
 import ReactDOM from "react-dom";
 import { Link, withRouter } from "react-router-dom";
 import Routes from "./Routes";
@@ -9,10 +11,12 @@ import "./index.css";
 import MyNavBar from './containers/MyNavBar';
 import MyAccord from './containers/MyAccord';
 import { Grid} from "react-bootstrap";
+import { Button,Accordion, Icon } from 'semantic-ui-react'
+//import '../sass/main.scss';
 //https://www.npmjs.com/package/react-split-pane
 
 
-var ReactGridLayout = require('react-grid-layout');
+//var ReactGridLayout = require('react-grid-layout');
 
 
 
@@ -23,10 +27,13 @@ class App extends Component {
 
     this.state = {
       isAuthenticated: false,
-      isAuthenticating: true
+      isAuthenticating: true,
+      activeIndex: 0
     };
         // This binding is necessary to make `this` work in the callback
     this.handleLogout = this.handleLogout.bind(this);
+        // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
 
   }
   async componentDidMount() {
@@ -52,6 +59,13 @@ class App extends Component {
     this.props.history.push("/login");
   }
 
+  handleClick = (e, titleProps) => {
+    const { index } = titleProps
+    const { activeIndex } = this.state
+    const newIndex = activeIndex === index ? -1 : index
+
+    this.setState({ activeIndex: newIndex })
+  }
 
   render() {
     const childProps = {
@@ -65,18 +79,60 @@ class App extends Component {
       {i: 'b', x: 1, y: 1, w: 3, h: 2, minW: 2, maxW: 4},
       {i: 'c', x: 4, y: 2, w: 1, h: 2}
     ];//https://github.com/STRML/react-grid-layout#usage
+        const { activeIndex } = this.state
     return (
 
-      <ReactGridLayout className="layout" layout={layout} cols={12} rowHeight={230} width={1200}>
-        <div key="a">
-
+    <SplitPane split="horizontal" allowResize={false} defaultSize={40}>
+        <div key="1">
+          <MyNavBar childProps={childProps} />
         </div>
-        <div key="b">
-                  <MyNavBar childProps={childProps} />
+        <SplitPane split="vertical" defaultSize={300}>
+            <div key="2" >
+      <Accordion fluid styled>
+        <Accordion.Title active={activeIndex === 0} index={0} onClick={this.handleClick}>
+          <Icon name='dropdown' />
+          What is a dog?
+        </Accordion.Title>
+        <Accordion.Content active={activeIndex === 0}>
+          <p>
+            A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found as a
+            {' '}welcome guest in many households across the world.
+          </p>
+        </Accordion.Content>
 
-        </div>
-        <div id='detail' key="c">c</div>
-      </ReactGridLayout>
+        <Accordion.Title active={activeIndex === 1} index={1} onClick={this.handleClick}>
+          <Icon name='dropdown' />
+          What kinds of dogs are there?
+        </Accordion.Title>
+        <Accordion.Content active={activeIndex === 1}>
+          <p>
+            There are many breeds of dogs. Each breed varies in size and temperament. Owners often select a breed of
+            {' '}dog that they find to be compatible with their own lifestyle and desires from a companion.
+          </p>
+        </Accordion.Content>
+
+        <Accordion.Title active={activeIndex === 2} index={2} onClick={this.handleClick}>
+          <Icon name='dropdown' />
+          How do you acquire a dog?
+        </Accordion.Title>
+        <Accordion.Content active={activeIndex === 2}>
+          <p>
+            Three common ways for a prospective owner to acquire a dog is from pet shops, private owners, or shelters.
+          </p>
+          <p>
+            A pet shop may be the most convenient way to buy a dog. Buying a dog from a private owner allows you to
+            {' '}assess the pedigree and upbringing of your dog before choosing to take it home. Lastly, finding your
+            {' '}dog from a shelter, helps give a good home to a dog who may not find one so readily.
+          </p>
+        </Accordion.Content>
+      </Accordion>
+            </div>
+            <div key="3" >
+              <Routes childProps={childProps} />
+               <div id='detail'></div>
+            </div>
+        </SplitPane>
+    </SplitPane>
 
     );
   }
--- src/containers/MyNavBar.js
+++ src/containers/MyNavBar.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Link, withRouter } from "react-router-dom";
-import { Nav,NavItem,Navbar,Row,Col,Accordion,Panel,ListGroup,ListGroupItem } from "react-bootstrap";
+import { Menu } from 'semantic-ui-react'
+//import { Nav,NavItem,Navbar,Row,Col,Accordion,Panel,ListGroup,ListGroupItem } from "react-bootstrap";
 import "../App.css";
 import Routes from "../Routes";
 import RouteNavItem from "../components/RouteNavItem";
@@ -13,11 +14,56 @@ class MyNavBar extends Component {
 
     this.state = {
     };
+        // This binding is necessary to make `this` work in the callback
+    this.handleItemClick = this.handleItemClick.bind(this);
+
   }
 
 
+  handleItemClick = (e, { name }) => {
+    this.setState({ activeItem: name });
+    this.props.history.push("/login");
+  }
+
   render() {
+    const { activeItem } = this.state
+
     return (
+      <Menu>
+        <Menu.Item
+          name='editorials'
+          active={activeItem === 'editorials'}
+          onClick={this.handleItemClick}
+        >
+          Editorials
+        </Menu.Item>
+
+        <Menu.Item
+          name='reviews'
+          active={activeItem === 'reviews'}
+          onClick={this.handleItemClick}
+        >
+          Reviews
+        </Menu.Item>
+
+        <Menu.Item
+          name='upcomingEvents'
+          active={activeItem === 'upcomingEvents'}
+          onClick={this.handleItemClick}
+        >
+          Upcoming Events
+        </Menu.Item>
+      </Menu>
+
+
+    );
+  }
+}
+
+export default withRouter(MyNavBar);
+
+
+/*
       <div className="App container">
         <Navbar inverse fluid collapseOnSelect>
           <Navbar.Header>
@@ -44,10 +90,5 @@ class MyNavBar extends Component {
         </Navbar>
 
       </div>
-    );
-  }
-}
 
-export default withRouter(MyNavBar);
-
-
+*/
